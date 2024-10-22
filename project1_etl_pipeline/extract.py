ebf9c7ff3e9fbf748bf61123e8f8bae942299984
Liashkov S <s.liashkov@twslabs.org>
2024-10-22 21:44:30 +0700

    made basic extract script

--- project1_etl_pipeline/extract.py
+++ project1_etl_pipeline/extract.py
@@ -4,11 +4,19 @@ import click
 import requests
 import logging
 import tqdm
+import datetime
+import json
 from pprint import pprint
+
+# TODO
+# 1) Use click
+# 2) Make api calls Asynchronous
+
+
 
 SOURCE_API_URL="https://randomuser.me/api/"
 FETCH_N_DOCS=10
-
+RETRIEVED_DATA_DIR="./data/"
 
 def fetch_user_from_api(url : str, timeout : int = 1):
     
@@ -36,5 +44,24 @@ def fetch_batch(url : str, number_of_users : int):
     
     return collected_users
 
-pprint(fetch_batch(url=SOURCE_API_URL, number_of_users=100))
+
+def load_batch_to_json(users, filepath):
     
+    batch = {
+        "users_list": users,
+        "number_users": len(users),
+        "batch_proccessed_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Human-readable timestamp
+    }
+
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(batch, f, ensure_ascii=False, indent=4)
+
+    print(f"Successfully saved {len(users)} users to {filepath}")
+
+
+
+if __name__ == "__main__":
+    
+    users = fetch_batch(url=SOURCE_API_URL, number_of_users=100)
+    
+    load_batch_to_json(users=users, filepath="./data/users_batch1.json")
